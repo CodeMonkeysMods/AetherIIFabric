b51aa72c11a3405fbfe3d74f9acb49600df7371a
Andy Wilson <andy.wilson1234@gmail.com>
2026-03-03 15:28:54 +0000

    feat: setup folders

--- src/main/java/com/tcm/AetherII/AetherII.java
+++ src/main/java/com/tcm/AetherII/AetherII.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.tcm.AetherII;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class AetherII implements ModInitializer {
 	public static final String MOD_ID = "aetherii";
 
 	// This logger is used to write text to the console and the log file.
